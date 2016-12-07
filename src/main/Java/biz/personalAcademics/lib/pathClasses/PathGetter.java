61f5c72e470353ea2891b1f186a156c50675bb6e
Kneuman0 <kylepneuman@gmail.com>
2016-12-06 22:12:57 -0500

    Pushing changes on local machine

--- src/main/Java/biz/personalAcademics/lib/pathClasses/PathGetter.java
+++ src/main/Java/biz/personalAcademics/lib/pathClasses/PathGetter.java
@@ -1,31 +1,49 @@
 package biz.personalAcademics.lib.pathClasses;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
-
+/**
+ * Available in both static and instance methods. These methods are made for use with jar files
+ * @author Karottop
+ *
+ */
 public class PathGetter {
 	
 	Object localClass;
 	
+	/**
+	 * Pass in a class that you want to use as a starting point for
+	 * your relative path.
+	 * @param localClass
+	 */
 	public PathGetter(Object localClass){
 		this.localClass = localClass;
 	}
 	
+	// if using static overloaded methods
+	public PathGetter(){
+		localClass = null;
+	}
+	
 	/**
-	 * returns an absolute File:/ URI in string form back to the subfolder containing the class that was
-	 * passed into the PathGetter object. String has File:/ string built in
+	 * returns an absolute File:/// URI in string form back to the subfolder containing the class that was
+	 * passed into the PathGetter object. String has File:/// string built in
 	 * 
 	 * If used in a runnable jar, the method will return an absolute path back to the jar file
 	 * 
 	 * String will end with a '/'
 	 * @return
+	 * @throws UnsupportedEncodingException 
 	 */
-	public String getFileAbsoluteSubfolderURIString(){
+	public String getFileAbsoluteSubfolderURIString() throws UnsupportedEncodingException{
 		File currentJavaJarFile = new File(localClass.getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
 		String currentJavaJarFilePath = currentJavaJarFile.getAbsolutePath();
 		String currentRootDirectoryPath = currentJavaJarFilePath.replace(
 				currentJavaJarFile.getName(), "").replace("\\", "/");
-		return "File:/" + currentRootDirectoryPath;
+		
+		return "File:///" + URLEncoder.encode(currentRootDirectoryPath, "UTF-8").replace("+", "%20");
 	}
 	
 	/**
@@ -59,6 +77,10 @@ public class PathGetter {
 		String currentRootDirectoryPath = currentJavaJarFilePath.replace(
 				currentJavaJarFile.getName(), "");
 		return currentRootDirectoryPath; 
+	}
+	
+	public String readThis(){
+		return "This String Right Here";
 	}
 	
 }
